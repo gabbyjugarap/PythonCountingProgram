0ffe9adea052d3f9dd9f69f61cd012a9e6c28d8c
Gabby Jugarap <84052296+gabbyjugarap@users.noreply.github.com>
2021-06-10 09:47:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 name=input("Enter name: ")
+print("\n")
 print("Hello " + name + "!")
-
+print("\n")
 print("Let's count by even numbers!")
-
+print("\n")
 start= int(input("Enter start of the range: "))
 end= int(input("Enter end of the range: "))
 for num in range(start, end +1):
   if num % 2 == 0:
     print(num)
-
+print("\n")
 print("Let's count by odd numbers!")
-
+print("\n")
 x= int(input("Enter start of range: "))
 y= int(input("Enter end of range: "))
 for z in range(x, y + 1):
